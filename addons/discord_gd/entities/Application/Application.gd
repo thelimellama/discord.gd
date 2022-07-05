75349bb5126deb3b38caf0e24f452e2f9234a56d
3ddelano <3ddelano@gmail.com>
2022-07-05 09:48:22 +0530

    feat: complete Application resource

--- addons/discord_gd/entities/Application/Application.gd
+++ addons/discord_gd/entities/Application/Application.gd
@@ -1,8 +1,5 @@
 # Represents a Discord Application
-class_name Application
-extends Dataclass
-
-func _init().("Application"): return self
+class_name Application extends Dataclass
 
 var id: String # The id of the app
 var name: String # The name of the app
@@ -13,10 +10,9 @@ var bot_public: bool # When false only app owner can join the app's bot to guild
 var bot_require_code_grant: bool # When true the app's bot will only join upon completion of the full oauth2 code grant flow
 var terms_of_service_url = null # [String] The url of the app's terms of service
 var privacy_policy_url = null # [String] The url of the app's privacy policy
-var owner = null # PartialUser Partial user object containing info on the owner of the application
-var summary: String # (deprecated) and will be removed in v11. An empty string.
+var owner = null # [User] Partial user object containing info on the owner of the application
 var verify_key: String # The hex encoded key for verification in interactions and the GameSDK's GetTicket
-var team = null # Team If the application belongs to a team, this will be a list of the members of that team
+var team = null # [Team] If the application belongs to a team, this will be a list of the members of that team
 var guild_id = null # [String] If this application is a game sold on Discord, this field will be the guild to which it has been linked
 var primary_sku_id = null # [String] If this application is a game sold on Discord, this field will be the id of the "Game SKU" that is created, if exists
 var slug = null # [String] If this application is a game sold on Discord, this field will be the URL slug that links to the store page
@@ -24,4 +20,29 @@ var cover_image = null # [String] The application's default rich presence invite
 var flags = null # [ApplicationFlags] The application's public flags
 var tags = null # [Array] of [String] Up to 5 tags describing the content and functionality of the application
 var install_params = null # [ApplicationInstallParams] Settings for the application's default in-app authorization link, if enabled
-var custom_install_url: String # The application's default custom authorization link, if enabled
+var custom_install_url = null # [String] The application's default custom authorization link, if enabled
+
+
+# @hidden
+func _init().("Application"): return self
+
+
+# @hidden
+func from_dict(p_dict: Dictionary):
+	.from_dict(p_dict)
+
+	icon = DiscordUitls.get_or_default(p_dict, "icon", null)
+	rpc_origins = DiscordUitls.get_or_default(p_dict, "rpc_origins", null)
+	terms_of_service_url = DiscordUitls.get_or_default(p_dict, "terms_of_service_url", null)
+	privacy_policy_url = DiscordUitls.get_or_default(p_dict, "privacy_policy_url", null)
+	if p_dict.has("owner"):	owner = User.new().from_dict(p_dict.owner)
+	if p_dict.has("team"): team = Team.new().from_dict(p_dict.team) 
+	guild_id = DiscordUitls.get_or_default(p_dict, "guild_id", null)
+	primary_sku_id = DiscordUitls.get_or_default(p_dict, "primary_sku_id", null)
+	slug = DiscordUitls.get_or_default(p_dict, "slug", null)
+	cover_image = DiscordUitls.get_or_default(p_dict, "cover_image", null)
+	if p_dict.has("flags"):	flags = ApplicationFlags.new(p_dict.flags)
+	tags = DiscordUitls.get_or_default(p_dict, "tags", null)
+	install_params = DiscordUitls.get_or_default(p_dict, "install_params", null)
+	custom_install_url = DiscordUitls.get_or_default(p_dict, "custom_install_url", null)
+
--- addons/discord_gd/entities/Application/ApplicationInstallParams.gd
+++ addons/discord_gd/entities/Application/ApplicationInstallParams.gd
@@ -1,7 +1,9 @@
-# Represents a Discord Application Install Params object
+# Represents a Discord application install params
 class_name ApplicationInstallParams extends Dataclass
-
-func _init().("ApplicationInstallParams"): return self
 
 var scopes: Array # [Array] of [String] [OAuth2Scopes] The scopes to add the application to the server with
 var permissions: String # The permissions to request for the bot role
+
+
+# @hidden
+func _init().("ApplicationInstallParams"): return self
